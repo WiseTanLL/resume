09ea38abc27d1527699fa31e67010183507663ff
tanjun <634096144@qq.com>
2017-01-16 17:41:05 +0800

    feature.新进展

--- apps/frontend/views/index/index.volt
+++ apps/frontend/views/index/index.volt
@@ -1,17 +1,52 @@
+
+<!-- 左边菜单栏 -->
+<div class="pt-page-left-top">
+	<span></span>
+	<span></span>
+	<span></span>
+</div>
+
+
 <div class="container">
-	<div class="section">
-		<img src="{{ imgs.test }}">
+	<div class="section pt-current-page">
+		
 	</div>
 	<div class="section">
-		<img src="{{ imgs.test }}">
+		
 	</div>
 	<div class="section">
-		<img src="{{ imgs.test }}">
+		
 	</div>
 	<div class="section">
-		<img src="{{ imgs.test }}">
+		
 	</div>
-	<div class="section">
-		<img src="{{ imgs.test }}">
+</div>
+
+<div class="pt-page-black">
+	<div class="pt-close"></div>
+	<div class="main">
+		<div class="item active">
+			<div class="text-align-right"><span class="font-size24">01</span></div>
+			<div><span>HOME</span></div>
+		</div>
+		<div class="item">
+			<div class="text-align-right"><span class="font-size24">02</span></div>
+			<div><span>ABOUT ME</span></div>
+		</div>
+		<div class="item">
+			<div class="text-align-right"><span class="font-size24">03</span></div>
+			<div><span>PORTFOLIO</span></div>
+		</div>
+		<div class="item">
+			<div class="text-align-right"><span class="font-size24">04</span></div>
+			<div><span>CONTACT</span></div>
+		</div>
 	</div>
+</div>
+
+<div class="pt-page-right">
+	<span class="active"></span>
+	<span></span>
+	<span></span>
+	<span></span>
 </div>
--- public/js/base.js
+++ public/js/base.js
@@ -3,22 +3,68 @@
  */
 
 $(function(){
-	$(window).on('mousewheel',function(e){
-		// if (e.wheelDelta) {  //判断浏览器IE，谷歌滑轮事件             
-  //           if (e.wheelDelta > 0) { //当滑轮向上滚动时
-  //               alert("滑轮向上滚动");
-  //           }
-  //           if (e.wheelDelta < 0) { //当滑轮向下滚动时
-  //               alert("滑轮向下滚动");
-  //           }
-  //       } else if (e.detail) {  //Firefox滑轮事件
-  //           if (e.detail> 0) { //当滑轮向上滚动时
-  //               alert("滑轮向上滚动");
-  //           }
-  //           if (e.detail< 0) { //当滑轮向下滚动时
-  //               alert("滑轮向下滚动");
-  //           }
-  //       }
-  			console.log(e);
+	window.canPageFlash = 1;//控制是否能检测滑动事件
+	window.currentPage = 0;//当前页面index
+
+	initEvent();
+
+	/* 菜单栏出现控制 */
+	$('.pt-page-left-top').on('click',function(){
+		$('.pt-page-black').fadeIn('fast',function(){
+			window.canPageFlash = 0;
+		});
 	})
+	$('.pt-page-black .pt-close').on('click',function(){
+		$('.pt-page-black').fadeOut('fast',function(){
+			window.canPageFlash = 1;
+		});
+	});
 })
+
+function initEvent(){
+	$(window).on('mousewheel DOMMouseScroll',function(e){
+		if (window.canPageFlash){
+			var delta = (e.originalEvent.wheelDelta && (e.originalEvent.wheelDelta > 0 ? 1 : -1)) ||  // chrome & ie
+	            (e.originalEvent.detail && (e.originalEvent.detail > 0 ? -1 : 1));              // firefox
+		    if (delta > 0) {  //向上滚
+		    	var pagePre = $('.pt-current-page').prev(),
+		    		pageNow = $('.pt-current-page');
+		       	if (pagePre.length){
+		       		window.canPageFlash = 0;
+		       		pagePre.addClass('pt-page-rotatePullTop pt-current-page');
+		       		pageNow.addClass('pt-page-rotatePushBottom');
+
+		       		$('.pt-page-right span').eq(window.currentPage-1).addClass('active').siblings().removeClass('active');
+		       		$('.pt-page-black .main .item').eq(window.currentPage-1).addClass('active').siblings().removeClass('active');
+
+		       		var timeout = setTimeout(function(){
+		       			pagePre.removeClass('pt-page-rotatePullTop');
+		       			pageNow.removeClass('pt-page-rotatePushBottom pt-current-page');
+		       			window.currentPage = $('.pt-current-page').index();
+		       			window.canPageFlash = 1;
+		       			clearTimeout(timeout);
+		       		},600);
+		       	}
+		    } else if (delta < 0) { //向下滚
+		        var pageNext = $('.pt-current-page').next(),
+		    		pageNow = $('.pt-current-page');
+		       	if (pageNext.length){
+		       		window.canPageFlash = 0;
+		       		pageNext.addClass('pt-page-rotatePullTop pt-current-page');
+		       		pageNow.addClass('pt-page-rotatePushBottom');
+
+		       		$('.pt-page-right span').eq(window.currentPage+1).addClass('active').siblings().removeClass('active');
+		       		$('.pt-page-black .main .item').eq(window.currentPage+1).addClass('active').siblings().removeClass('active');
+
+		       		var timeout = setTimeout(function(){
+		       			pageNext.removeClass('pt-page-rotatePullTop');
+		       			pageNow.removeClass('pt-page-rotatePushBottom pt-current-page');
+		       			window.currentPage = $('.pt-current-page').index();
+		       			window.canPageFlash = 1;
+		       			clearTimeout(timeout);
+		       		},600);
+		       	}
+		    }
+		}
+	});
+}
